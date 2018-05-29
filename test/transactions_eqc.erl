f5d85a00fca530f9f2776f0468e7c12c42711cc4
Thomas Arts <thomas.arts@quviq.com>
2018-05-29 12:28:08 +0200

    choose an amount less than balance - fee
    Interesting error shows up:
    .Failed! Reason:
    {'EXIT',{{choose,1,-2},[]}}
    Error in test case generation and shrinking therefore does not work.

--- test/transactions_eqc.erl
+++ test/transactions_eqc.erl
@@ -65,7 +65,7 @@ spend_pre(S) ->
 
 spend_args(S) ->
   ?LET({From, Fee, To}, {elements(S#state.accounts), choose(1,5), elements(S#state.accounts)},
-       [From, int(), Fee, To#account.pubkey ]).
+       [From, choose(1, From#account.balance - Fee), Fee, To#account.pubkey ]).
 
 
 spend_pre(_S, [From, Amount, Fee, To]) ->
